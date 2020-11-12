f700f651fe2cce8ad363fb5e505427b31d1f0d17
Chmil Ruslan <chmilrv@users.noreply.github.com>
2020-11-12 20:51:34 +0200

    2051

--- DZ_PT_WinForms_2_1/Form1.Designer.cs
+++ DZ_PT_WinForms_2_1/Form1.Designer.cs
@@ -33,26 +33,24 @@
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.progressBar2 = new System.Windows.Forms.ProgressBar();
-            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(8, 368);
+            this.progressBar1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.progressBar1.Location = new System.Drawing.Point(8, 522);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(576, 23);
+            this.progressBar1.Size = new System.Drawing.Size(659, 23);
             this.progressBar1.TabIndex = 0;
             // 
             // richTextBox1
             // 
             this.richTextBox1.Location = new System.Drawing.Point(8, 8);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
-            this.richTextBox1.Size = new System.Drawing.Size(784, 336);
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox1.Size = new System.Drawing.Size(765, 500);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
-            this.richTextBox1.VScroll += new System.EventHandler(this.richTextBox1_VScroll);
             // 
             // openFileDialog1
             // 
@@ -60,9 +58,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(680, 368);
+            this.button1.Location = new System.Drawing.Point(673, 522);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(115, 23);
+            this.button1.Size = new System.Drawing.Size(100, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "Открыть файл";
             this.button1.UseVisualStyleBackColor = true;
@@ -71,42 +69,28 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(600, 376);
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(180)))), ((int)(((byte)(0)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(319, 525);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(45, 16);
             this.label1.TabIndex = 3;
             this.label1.Text = "label1";
-            // 
-            // progressBar2
-            // 
-            this.progressBar2.Location = new System.Drawing.Point(40, 416);
-            this.progressBar2.Name = "progressBar2";
-            this.progressBar2.Size = new System.Drawing.Size(448, 23);
-            this.progressBar2.TabIndex = 4;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(560, 416);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "label2";
+            this.label1.UseMnemonic = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(807, 450);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.progressBar2);
+            this.ClientSize = new System.Drawing.Size(780, 557);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.progressBar1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "DZ_PT_WinForms_2_1";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -120,8 +104,6 @@
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ProgressBar progressBar2;
-        private System.Windows.Forms.Label label2;
     }
 }
 
--- DZ_PT_WinForms_2_1/Form1.cs
+++ DZ_PT_WinForms_2_1/Form1.cs
@@ -1,14 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-
 namespace DZ_PT_WinForms_2_1
 {
     public partial class Form1 : Form
@@ -16,53 +9,31 @@ namespace DZ_PT_WinForms_2_1
         public Form1()
         {
             InitializeComponent();
-            label1.Text = "0 %";
-
-
-
-
+            label1.Text = string.Empty;
         }
-
-        public void LoadMyFile()
+        public void LoadFile()
         {
             OpenFileDialog openFile1 = new OpenFileDialog();
-            //openFile1.DefaultExt = "*.rtf";
-            //openFile1.Filter = "RTF Files|*.rtf";
-
+            openFile1.DefaultExt = "*.rtf";
+            openFile1.Filter = "RTF Files|*.rtf";
             if (openFile1.ShowDialog() == DialogResult.OK && openFile1.FileName.Length > 0)
             {
                 richTextBox1.LoadFile(openFile1.FileName);
             }
         }
-
-
-
         private void button1_Click(object sender, EventArgs e)
         {
-
-            LoadMyFile();
-
-
+            LoadFile();
             progressBar1.Minimum = 0;
             progressBar1.Maximum = 100;
             progressBar1.Step = 5;
-
             for (int i = progressBar1.Minimum; i <= progressBar1.Maximum; i++)
             {
                 progressBar1.PerformStep();
-                label1.Text = progressBar1.Value.ToString() + " %";
-                this.Update();
-                Thread.Sleep(100);
+                label1.Text = progressBar1.Value.ToString() + "%";
+                Update();
+                Thread.Sleep(10);
             }
-        }
-
-        private void richTextBox1_VScroll(object sender, EventArgs e)
-        {
-            //label2.Text = e.ToString();
-
-            MessageBox.Show(e.ToString());
-
-
         }
     }
 }
