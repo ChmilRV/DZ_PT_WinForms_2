357b220bb1c5a4439fc4b0a73f391143e9776c71
chmilrv@gmail.com <chmilrv@gmail.com>
2020-11-13 13:13:30 +0200

    1313

--- DZ_PT_WinForms_2_2/Form1.Designer.cs
+++ DZ_PT_WinForms_2_2/Form1.Designer.cs
@@ -38,13 +38,16 @@
             this.label_PhoneNumber = new System.Windows.Forms.Label();
             this.textBox_PhoneNumber = new System.Windows.Forms.TextBox();
             this.listBox_UserInfo = new System.Windows.Forms.ListBox();
-            this.groupBox_ = new System.Windows.Forms.GroupBox();
-            this.button_AddUser = new System.Windows.Forms.Button();
             this.contextMenuStrip_EditDelete = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.Edit_ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.Delete_ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.groupBox_.SuspendLayout();
+            this.groupBox_ = new System.Windows.Forms.GroupBox();
+            this.button_AddUser = new System.Windows.Forms.Button();
+            this.contextMenuStrip_About = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.contextMenuStrip_EditDelete.SuspendLayout();
+            this.groupBox_.SuspendLayout();
+            this.contextMenuStrip_About.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox_Surname
@@ -77,7 +80,7 @@
             this.textBox_Name.Location = new System.Drawing.Point(77, 65);
             this.textBox_Name.Name = "textBox_Name";
             this.textBox_Name.Size = new System.Drawing.Size(127, 20);
-            this.textBox_Name.TabIndex = 0;
+            this.textBox_Name.TabIndex = 1;
             // 
             // label_Email
             // 
@@ -93,7 +96,7 @@
             this.textBox_Email.Location = new System.Drawing.Point(77, 106);
             this.textBox_Email.Name = "textBox_Email";
             this.textBox_Email.Size = new System.Drawing.Size(127, 20);
-            this.textBox_Email.TabIndex = 0;
+            this.textBox_Email.TabIndex = 2;
             // 
             // label_PhoneNumber
             // 
@@ -109,15 +112,38 @@
             this.textBox_PhoneNumber.Location = new System.Drawing.Point(77, 145);
             this.textBox_PhoneNumber.Name = "textBox_PhoneNumber";
             this.textBox_PhoneNumber.Size = new System.Drawing.Size(127, 20);
-            this.textBox_PhoneNumber.TabIndex = 0;
+            this.textBox_PhoneNumber.TabIndex = 3;
             // 
             // listBox_UserInfo
             // 
+            this.listBox_UserInfo.ContextMenuStrip = this.contextMenuStrip_EditDelete;
             this.listBox_UserInfo.FormattingEnabled = true;
             this.listBox_UserInfo.Location = new System.Drawing.Point(249, 12);
             this.listBox_UserInfo.Name = "listBox_UserInfo";
             this.listBox_UserInfo.Size = new System.Drawing.Size(323, 225);
             this.listBox_UserInfo.TabIndex = 2;
+            // 
+            // contextMenuStrip_EditDelete
+            // 
+            this.contextMenuStrip_EditDelete.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Edit_ToolStripMenuItem,
+            this.Delete_ToolStripMenuItem});
+            this.contextMenuStrip_EditDelete.Name = "contextMenuStrip_EditDelete";
+            this.contextMenuStrip_EditDelete.Size = new System.Drawing.Size(181, 70);
+            // 
+            // Edit_ToolStripMenuItem
+            // 
+            this.Edit_ToolStripMenuItem.Name = "Edit_ToolStripMenuItem";
+            this.Edit_ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.Edit_ToolStripMenuItem.Text = "Редактировать";
+            this.Edit_ToolStripMenuItem.Click += new System.EventHandler(this.Edit_ToolStripMenuItem_Click);
+            // 
+            // Delete_ToolStripMenuItem
+            // 
+            this.Delete_ToolStripMenuItem.Name = "Delete_ToolStripMenuItem";
+            this.Delete_ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.Delete_ToolStripMenuItem.Text = "Удалить";
+            this.Delete_ToolStripMenuItem.Click += new System.EventHandler(this.Delete_ToolStripMenuItem_Click);
             // 
             // groupBox_
             // 
@@ -147,43 +173,37 @@
             this.button_AddUser.UseVisualStyleBackColor = true;
             this.button_AddUser.Click += new System.EventHandler(this.button_AddUser_Click);
             // 
-            // contextMenuStrip_EditDelete
+            // contextMenuStrip_About
             // 
-            this.contextMenuStrip_EditDelete.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.Edit_ToolStripMenuItem,
-            this.Delete_ToolStripMenuItem});
-            this.contextMenuStrip_EditDelete.Name = "contextMenuStrip_EditDelete";
-            this.contextMenuStrip_EditDelete.Size = new System.Drawing.Size(155, 48);
-            this.contextMenuStrip_EditDelete.Text = "rtrtrrt";
+            this.contextMenuStrip_About.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.contextMenuStrip_About.Name = "contextMenuStrip_About";
+            this.contextMenuStrip_About.Size = new System.Drawing.Size(108, 26);
+            this.contextMenuStrip_About.Click += new System.EventHandler(this.contextMenuStrip_About_Click);
             // 
-            // Edit_ToolStripMenuItem
+            // aboutToolStripMenuItem
             // 
-            this.Edit_ToolStripMenuItem.Name = "Edit_ToolStripMenuItem";
-            this.Edit_ToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
-            this.Edit_ToolStripMenuItem.Text = "Редактировать";
-            this.Edit_ToolStripMenuItem.Click += new System.EventHandler(this.редактироватьToolStripMenuItem_Click);
-            // 
-            // Delete_ToolStripMenuItem
-            // 
-            this.Delete_ToolStripMenuItem.Name = "Delete_ToolStripMenuItem";
-            this.Delete_ToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
-            this.Delete_ToolStripMenuItem.Text = "Удалить";
-            this.Delete_ToolStripMenuItem.Click += new System.EventHandler(this.удалитьToolStripMenuItem_Click);
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 441);
+            this.ContextMenuStrip = this.contextMenuStrip_About;
             this.Controls.Add(this.button_AddUser);
             this.Controls.Add(this.groupBox_);
             this.Controls.Add(this.listBox_UserInfo);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "DZ_PT_WinForms_2_2";
+            this.contextMenuStrip_EditDelete.ResumeLayout(false);
             this.groupBox_.ResumeLayout(false);
             this.groupBox_.PerformLayout();
-            this.contextMenuStrip_EditDelete.ResumeLayout(false);
+            this.contextMenuStrip_About.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -204,6 +224,8 @@
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip_EditDelete;
         private System.Windows.Forms.ToolStripMenuItem Edit_ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem Delete_ToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip_About;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
     }
 }
 
--- DZ_PT_WinForms_2_2/Form1.cs
+++ DZ_PT_WinForms_2_2/Form1.cs
@@ -19,20 +19,106 @@ namespace DZ_PT_WinForms_2_2
 
         private void button_AddUser_Click(object sender, EventArgs e)
         {
-
-
-
-
+            String userFullInfo = String.Empty;
+            userFullInfo = textBox_Surname.Text + " " + textBox_Name.Text + ", " + textBox_Email.Text + ", " + textBox_PhoneNumber.Text;
+            if (!String.IsNullOrEmpty(userFullInfo))
+            {
+                if (!listBox_UserInfo.Items.Contains(userFullInfo))
+                {
+                    listBox_UserInfo.Items.Add(userFullInfo);
+                }
+                else
+                    MessageBox.Show("Strings must be unique");
+            }
+            else
+                MessageBox.Show("Empty string");
+            textBox_Surname.Text = string.Empty;
+            textBox_Name.Text = string.Empty;
+            textBox_Email.Text = string.Empty;
+            textBox_PhoneNumber.Text = string.Empty;
         }
 
-        private void редактироватьToolStripMenuItem_Click(object sender, EventArgs e)
+        
+
+        private void Edit_ToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            //textBox_Surname.Text = string.Empty;
+            //textBox_Name.Text = string.Empty;
+            //textBox_Email.Text = string.Empty;
+            //textBox_PhoneNumber.Text = string.Empty;
+
+            if (this.listBox_UserInfo.Items.Count != 0)
+            {
+                if (this.listBox_UserInfo.SelectedItem != null)
+                {
+                    String userFullInfo= (String)listBox_UserInfo.SelectedItem;
+
+                    textBox_Surname.Text = userFullInfo.Remove(userFullInfo.IndexOf(' '));
+
+                    textBox_Name.Text = userFullInfo.Substring(userFullInfo.IndexOf(' ') + 1, userFullInfo.IndexOf(',') - userFullInfo.IndexOf(' ') - 1);
+
+                    //textBox_Email.Text =;
+
+                    //textBox_PhoneNumber =;
+                    listBox_UserInfo.Items.Remove(userFullInfo);
+
+
+                }
+            }
+
+
+            
+
+
+
+
+
+
 
         }
 
-        private void удалитьToolStripMenuItem_Click(object sender, EventArgs e)
+
+
+
+        private void Delete_ToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            if (this.listBox_UserInfo.Items.Count != 0)
+            {
+                if (this.listBox_UserInfo.SelectedItems != null)
+                {
+                    for (int i = 0; i < this.listBox_UserInfo.SelectedItems.Count; i++)
+                    {
+                        this.listBox_UserInfo.Items.Remove(this.listBox_UserInfo.SelectedItems[i]);
+                        i--;
+                    }
+                }
+            }
+
+
 
         }
+
+
+        private void contextMenuStrip_About_Click(object sender, EventArgs e)
+        {
+            contextMenuStrip_About.Visible = false;
+
+            MessageBox.Show("Приложение – анкета.\n" +
+                "Кнопка «Add» добавляет пользователя в окно хранения информации.\n" +
+                "В окне хранения через контекстное меню реализовано удаление пользователя и редактирование.\n" +
+                "экспорт / импорт всей информации о пользователях в текстовый файл.\n" +
+                "экспорт / импорт всей информации о пользователях в *.xml файл.",
+                "About",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+
+
+        }
+
+
+
+
+
+
     }
 }
